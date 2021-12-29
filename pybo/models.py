e4ed6a7cad68622ba05040c29d4eeaacb0d7d15d
jeongmun <lot9616@gmail.com>
2021-12-29 18:09:00 +0900

    3-12

--- pybo/models.py
+++ pybo/models.py
@@ -10,21 +10,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
-	author = models.ForeignKey(User, on_delete=models.CASCADE)  # on-delete 옵션 :계정이 삭제되면 이 계정이 작성한 질문을 모두 삭제
+	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')  # on-delete 옵션 :계정이 삭제되면 이 계정이 작성한 질문을 모두 삭제
 	subject = models.CharField(max_length=200)  # 글자 수가 제한된 필드의 경우 CharField 사용
 	content = models.TextField()                # 글자 수를 제한할 수 없을 땐 TextField 사용
 	create_date = models.DateTimeField()
 	modify_date = models.DateTimeField(null=True, blank=True)
-
+	voter = models.ManyToManyField(User, related_name='voter_question') # 추천인 추가
 	def __str__(self):
 		return self.subject
 
 class Answer(models.Model):
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
 	question = models.ForeignKey(Question, on_delete=models.CASCADE) #CASCADE는 이 답변과 연결된 질문이 삭제될 경우 함께 삭제된다는 의미(종속된다는 의미)
 	content = models.TextField()
 	create_date = models.DateTimeField()
 	modify_date = models.DateTimeField(null=True, blank=True) # blank=True 의미 : form.is_valid() 유효성 검사 없어도 된다는 의미
+	voter = models.ManyToManyField(User, related_name='voter_answer')
 
 
 class Comment(models.Model):
